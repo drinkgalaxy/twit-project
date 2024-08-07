c4e9463be0539c609821840395b24435d70e69ee
drinkgalaxy <drinkgalaxy1226@gmail.com>
2024-08-07 23:29:53 +0900

    commit : post 테스트 코드 작성 완료

--- src/test/java/com/example/myproject/domain/post/service/PostServiceTest.java
+++ src/test/java/com/example/myproject/domain/post/service/PostServiceTest.java
@@ -2,33 +2,28 @@ package com.example.myproject.domain.post.service;
 
 import com.example.myproject.domain.comment.entity.Comment;
 import com.example.myproject.domain.comment.repository.CommentRepository;
+import com.example.myproject.domain.multipart.entity.Multipart;
 import com.example.myproject.domain.multipart.repository.MultipartRepository;
 import com.example.myproject.domain.multipart.service.LocalFileStorageService;
 import com.example.myproject.domain.post.dto.PostCreateRequest;
 import com.example.myproject.domain.post.dto.PostResponse;
+import com.example.myproject.domain.post.dto.PostUpdateRequest;
 import com.example.myproject.domain.post.entity.Post;
 import com.example.myproject.domain.post.repository.PostRepository;
+import com.example.myproject.domain.scrap.entity.Scrap;
 import com.example.myproject.domain.scrap.repository.ScrapRepository;
 import com.example.myproject.domain.user.entity.User;
 import com.example.myproject.domain.user.enumtype.Role;
 import com.example.myproject.domain.user.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.*;
-import org.springframework.security.core.parameters.P;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -213,8 +208,6 @@ class PostServiceTest {
     void findMyComments() {
         // given
         Long userId = 1L;
-        Long commentId = 1L;
-
         // 댓글 목록
         Comment comment1 = mock(Comment.class);
         Comment comment2 = mock(Comment.class);
@@ -269,22 +262,129 @@ class PostServiceTest {
     }
 
     @Test
+    @DisplayName("본인이 스크랩한 게시글 목록 조회")
     void findScraps() {
+        // given
+        Long userId = 1L;
+        // 스크랩 목록
+        Scrap scrap1 = mock(Scrap.class);
+        Scrap scrap2 = mock(Scrap.class);
+
+        // 스크랩한 게시글
+        Long postId1 = 1L;
+        Long postId2 = 2L;
+        Post post1 = mock(Post.class);
+        Post post2 = mock(Post.class);
+
+        PostResponse response1 = PostResponse.builder()
+                .id(postId1)
+                .userId(userId)
+                .title("제목")
+                .contents("내용")
+                .description("한줄소개")
+                .viewCount(1L)
+                .use_yn(true)
+                .createdBy("닉네임")
+                .lastModifiedBy("닉네임")
+                .fileName("파일 이름")
+                .build();
+
+        PostResponse response2 = PostResponse.builder()
+                .id(postId2)
+                .userId(userId)
+                .title("제목")
+                .contents("내용")
+                .description("한줄소개")
+                .viewCount(1L)
+                .use_yn(true)
+                .createdBy("닉네임")
+                .lastModifiedBy("닉네임")
+                .fileName("파일 이름")
+                .build();
+
+        // Mock 설정
+        when(scrap1.getPostId()).thenReturn(postId1);
+        when(scrap2.getPostId()).thenReturn(postId2);
+        when(scrapRepository.findAllByUserId(userId)).thenReturn(Arrays.asList(scrap1, scrap2));
+        when(postRepository.findByIdAble(postId1)).thenReturn(post1);
+        when(postRepository.findByIdAble(postId2)).thenReturn(post2);
+        when(post1.toResponse()).thenReturn(response1);
+        when(post2.toResponse()).thenReturn(response2);
+
+        // when
+        List<PostResponse> result = postService.findScraps(userId);
+
+        // then
+        assertNotNull(result);
+        assertEquals(2, result.size());
     }
 
     @Test
-    void update() {
+    @DisplayName("게시글 수정")
+    void update() throws IOException {
+        // given
+        Long userId = 1L;
+        Long postId = 1L;
+
+        MockMultipartFile file = new MockMultipartFile("서버에 저장된 새로운 파일", "새로운 파일", "text/plain", "Test content".getBytes());
+
+        Post savedPost = Post.builder()
+                .id(postId)
+                .userId(userId)
+                .title("제목")
+                .description("한줄소개")
+                .contents("내용")
+                .fileName("파일 이름")
+                .build();
+
+        PostUpdateRequest request = PostUpdateRequest.builder()
+                .description("새로운 한줄소개")
+                .contents("새로운 내용")
+                .attachFile(file)
+                .build();
+
+        Multipart multipart = Multipart.builder()
+                .postId(postId)
+                .originalFilename(file.getOriginalFilename())
+                .storedFilename(file.getName())
+                .build();
+
+        // Mock 설정
+        when(localFileStorageService.saveFile(postId, file)).thenReturn(multipart);
+        when(multipartRepository.save(multipart)).thenReturn(multipart);
+        when(postRepository.findByIdAble(postId)).thenReturn(savedPost);
+        savedPost.update(request.getContents(), request.getDescription(), multipart.getOriginalFileName());
+
+        // when
+        Post updatedPost = postService.update(postId, request, file);
+
+        // then
+        assertNotNull(updatedPost);
+        assertEquals(request.getContents(), updatedPost.getContents());
+        assertEquals(request.getDescription(), updatedPost.getDescription());
+        assertEquals(multipart.getOriginalFileName(), updatedPost.getFileName());
     }
 
     @Test
     void disablePostById() {
-    }
+        // given
+        Long userId = 1L;
 
-    @Test
-    void getPostAuthorId() {
-    }
+        Post savedPost = Post.builder()
+                .id(1L)
+                .userId(userId)
+                .title("제목")
+                .description("한줄소개")
+                .contents("내용")
+                .use_yn(true)
+                .build();
 
-    @Test
-    void getPostAuthorAuth() {
+        doNothing().when(postRepository).disablePostById(savedPost.getId());
+
+        // when
+        postService.disablePostById(savedPost.getId());
+
+        // then
+        verify(postRepository).disablePostById(savedPost.getId());
     }
 }
